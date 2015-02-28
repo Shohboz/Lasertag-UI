b489427197c77c635071f16c7d3a113b3f9bdef5
Shohboz <vip.shohboz@gmail.com>
2015-03-01 02:53:26 +0300

    Fix_column_name migration, add json to index GET.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,9 @@
 class GamesController < ApplicationController
+	before_action :set_game, only: [:show, :edit, :update, :destroy]
+
+	# GET /games
+	# GET /games.json
+	def index
+	  @games = Game.all
+	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150228232730) do
+ActiveRecord::Schema.define(version: 20150228233642) do
 
   create_table "crews", force: :cascade do |t|
     t.string   "name"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20150228232730) do
   end
 
   create_table "games", force: :cascade do |t|
-    t.datetime "startAt"
+    t.datetime "start_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
